8e19d3305179ad2b150e77d1f573c152f490c914
Clinton <Clintonm.pro@gmail.com>
2021-02-16 00:04:29 +0100

    Updates

---
+++ precache-manifest.7c10274b2d9cca2b272825206bd9b767.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "273a21e32458903eef80c8747b1e3260",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "68ec08ee5e7403decafe",
+    "url": "/portfolio/static/css/2.46018156.chunk.css"
+  },
+  {
+    "revision": "3714ec9e3399592d1876",
+    "url": "/portfolio/static/css/main.7f8305a9.chunk.css"
+  },
+  {
+    "revision": "68ec08ee5e7403decafe",
+    "url": "/portfolio/static/js/2.1c0ff3bc.chunk.js"
+  },
+  {
+    "revision": "a80b0387134ff115e52515fc5fa21755",
+    "url": "/portfolio/static/js/2.1c0ff3bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3714ec9e3399592d1876",
+    "url": "/portfolio/static/js/main.66807ed6.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "0030ec634d437f3c8ab3e8f14c7eec4f",
+    "url": "/portfolio/static/media/Hieroglyphs.0030ec63.jpg"
+  },
+  {
+    "revision": "85d370dcdcdcb47995459a8b184939a3",
+    "url": "/portfolio/static/media/Laptop2.85d370dc.jpg"
+  },
+  {
+    "revision": "67af05e12311bac06327b43ca31e166b",
+    "url": "/portfolio/static/media/earth.67af05e1.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/portfolio/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/portfolio/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/portfolio/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "621bd386841f74e0053cb8e67f8a0604",
+    "url": "/portfolio/static/media/ionicons.621bd386.svg"
+  },
+  {
+    "revision": "8a993a5dea30b9eab8d1f05c75fef6bc",
+    "url": "/portfolio/static/media/laptop.8a993a5d.jpg"
+  },
+  {
+    "revision": "bd80794fddbffb4031fab537cc898680",
+    "url": "/portfolio/static/media/myImage.bd80794f.png"
+  },
+  {
+    "revision": "aa9d6bdf267c30e67139306f0bac12cb",
+    "url": "/portfolio/static/media/pngme.aa9d6bdf.png"
+  },
+  {
+    "revision": "b2ae5652afc42615d766522ee5cff707",
+    "url": "/portfolio/static/media/pngme2.b2ae5652.png"
+  }
+]);
